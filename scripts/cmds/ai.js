c5c0a39293bc9eceb707d2605d4ebe0ee5a8529a
kyletekser04ph <kivlynx42@gmail.com>
2024-08-13 07:19:05 +0800

    Update and rename 000ai.js to ai.js

---
+++ scripts/cmds/ai.js
@@ -0,0 +1,81 @@
+const axios = require('axios');
+
+const Prefixes = [
+  'gpt',
+  'ai',
+  'what',
+  'Zeph',
+  'give',
+  'zep',
+];
+
+module.exports = {
+  config: {
+    name: 'ai',
+    version: '2.5.4',
+    author: 'Kylepogi',//credits owner of this api
+    role: 0,
+    category: 'ai',
+    shortDescription: {
+      en: 'Asks an AI for an answer.',
+    },
+    longDescription: {
+      en: 'Asks an AI for an answer based on the user prompt.',
+    },
+    guide: {
+      en: '{pn} [prompt]',
+    },
+  },
+
+  langs: {
+    en: {
+      final: "𓃵 • 𝗭𝗘𝗣𝗛 𝗚𝗣𝗧𝗩𝟮 👨🏻‍🏫",
+      loading: "⏱️𝘄𝗮𝗶𝘁 𝗳𝗼𝗿 𝘁𝗵𝗲 𝗮𝗻𝘀𝘄𝗲𝗿... "
+    }
+  },
+
+  onStart: async function () {},
+
+  onChat: async function ({ api, event, args, getLang, message }) {
+    try {
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+
+      if (!prefix) {
+        return;
+      }
+
+      const prompt = event.body.substring(prefix.length).trim();
+
+      if (prompt === '') {
+
+        await message.reply(
+          "💁🏻‍♂️ provide your question..."
+        );
+        
+        return;
+      }
+
+      const loadingMessage = getLang("loading");
+      const loadingReply = await message.reply(loadingMessage);
+      const url = "https://hercai.onrender.com/v3/hercai"; // Replace with the new API endpoint
+      const response = await axios.get(`${url}?question=${encodeURIComponent(prompt)}`);
+
+      if (response.status !== 200 || !response.data) {
+        throw new Error('Invalid or missing response from API');
+      }
+
+      const messageText = response.data.reply.trim(); // Adjust according to the response structure of the new API
+      const userName = getLang("final");
+      const finalMsg = `${userName}\n\n👨🏻‍🏫𝗔𝗻𝘀𝘄𝗲𝗿:  ${messageText}\n`;
+     api.editMessage(finalMsg, loadingReply.messageID); 
+
+      console.log('Sent answer as a reply to user');
+    } catch (error) {
+      console.error(`Failed to get answer: ${error.message}`);
+      api.sendMessage(
+        `${error.message}.\n\nYou can try typing your question again or resending it, as there might be a bug from the server that's causing the problem. It might resolve the issue.`,
+        event.threadID
+      );
+    }
+  },
+};
